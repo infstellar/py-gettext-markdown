eaffa11cbc397db14c3e3c46edf8f88867bea18e
infstellar <98541813+infstellar@users.noreply.github.com>
2023-04-20 00:08:04 +0800

    upd

--- generate_pot.py
+++ generate_pot.py
@@ -4,24 +4,32 @@ import os
 
 class GeneratePot():
     LANG='zh_CN'
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.origin_file = open(file_path, 'r').read()
-        self.text_list = re.split('  \n|\n\n|<br>',self.origin_file)
-        self.text_list = [i.replace('\n', '\\n') for i in self.text_list]
-        self.text_list = [i.replace('\"', '\\"') for i in self.text_list]
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        
 
-    def _write_file(self):
-        f = open(self.file_path.replace('.md','.py'), 'w')
+    def _write_file(self, file_path):
+        origin_file = open(file_path, 'r').read()
+        text_list = re.split('  \n|\n\n|<br>',origin_file)
+        text_list = [i.replace('\n', '\\n') for i in text_list]
+        text_list = [i.replace('\"', '\\"') for i in text_list]
+        f = open(file_path.replace('.md','.py'), 'w')
         f.write(f'import gettext\n')
-        f.write(f'l10n = gettext.translation("{self.LANG}", localedir = r"translation/locale", languages=["{self.LANG}"])\n')
+        f.write(f'l10n = gettext.translation("{self.LANG}", localedir = r"markdown_i18n/locale", languages=["{self.LANG}"])\n')
         f.write('l10n.install()\n')
         f.write(f'_ = l10n.gettext\n')
-        for i in self.text_list:
-            f.write(f'_("{i}")\n')
+        f.write(f'f = open(r"{file_path}")\n')
+        for i in text_list:
+            f.write(f'f.write(_("{i}"))\n')
+        f.write(f'f.close()')
         f.close()
-            
+    
+    def run(self):
+        for root, dirs, files in os.walk(self.folder_path):
+            for f in files:
+                if f.split('.')[-1] in ['md', 'markdown']:
+                    self._write_file(f"{root}/{f}")
       
 if __name__ == "__main__":
-    gp = GeneratePot('test.md')
-    gp._write_file()
+    gp = GeneratePot('md_test')
+    gp.run()
